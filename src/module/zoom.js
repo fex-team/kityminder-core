06150484d7ca7902e535aac2d133e2ecef5575b4
techird <techird@qq.com>
2015-01-04 17:59:24 +0800

    添加默认缩放等级

--- src/module/zoom.js
+++ src/module/zoom.js
@@ -148,6 +148,9 @@ define(function(require, exports, module) {
         return {
             init: function() {
                 this._zoomValue = 100;
+                this.addDefaultOption({
+                    zoom: [10, 20, 50, 100, 200]
+                });
                 setTextRendering();
             },
             commands: {
