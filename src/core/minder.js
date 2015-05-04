3d697b860118621286ae32b4c7b119735d1128ba
zhangbobell <zhangbobell@163.com>
2015-05-04 13:42:18 +0800

    fixed bower.json

--- src/core/minder.js
+++ src/core/minder.js
@@ -30,7 +30,7 @@ define(function(require, exports, module) {
         }
     });
 
-    Minder.version = '1.4.1';
+    Minder.version = '1.4.3';
 
     Minder.registerInitHook = function(hook) {
         _initHooks.push(hook);
