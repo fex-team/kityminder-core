41767e33f692f4fdce83a50e560c29b66a58db7e
张博 <zhangbo21@baidu.com>
2015-12-15 16:06:13 +0800

    changed version

--- src/core/minder.js
+++ src/core/minder.js
@@ -30,7 +30,7 @@ define(function(require, exports, module) {
         }
     });
 
-    Minder.version = '1.4.25';
+    Minder.version = '1.4.26';
 
     Minder.registerInitHook = function(hook) {
         _initHooks.push(hook);
